fcbb36c31762a914f2d691c43dbcf751956b15ba
Holly Wilkalis <holly.wilkalis@gmail.com>
2018-01-12 08:19:22 -0800

    Initial commit with empty files using my own build template

---
+++ README.md
@@ -0,0 +1,10 @@
+When cloning the project, run these commands before using:
+$ npm install
+$ bower install
+
+When ready to get the project running:
+$ gulp build
+$ gulp serve
+
+To test:
+$ npm test
---
+++ spec/project-spec.js
@@ -0,0 +1,12 @@
+describe('Triangle', function() {
+
+  var reusableTriangle;
+
+  beforeEach(function() {
+    reusableTriangle = new Triangle(5, 5, 5);
+  });
+  
+  it('should test whether a Triangle has three sides', function() {
+    //Test content will go here.
+  });
+});
